a2291ae913f10d59a774aaa757360e123cdf87ff
descnz <90636380+descnz@users.noreply.github.com>
2022-07-01 21:33:53 -0500

    Update handleError.js
    Agregacion del "success: false" en el manejador de errores

--- utils/handleError.js
+++ utils/handleError.js
@@ -1,6 +1,6 @@
 const handleHttpError= (res, message= 'Algo sucedio...', code= 403) =>{  //parametros de arriba, son los de default
     res.status(code);
-    res.send    ({error: message});
+    res.send    ({error: message, sucess: false});
 };
 
 module.exports= {handleHttpError};
