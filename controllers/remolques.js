bfbd3100a2126d2e61450a1e264a21ba18674375
descnz <90636380+descnz@users.noreply.github.com>
2022-09-15 21:08:31 -0500

    tbl_remolques: read Remolques, Bug fixed

--- controllers/remolques.js
+++ controllers/remolques.js
@@ -82,7 +82,7 @@ const readRemolqueCtrl = async (req, res) => {
         //"`docs`.`url_TarjetaCirculacion`, `docs`.`url_Factura` , `docs`.`url_PermisoSCT`,`docs`.`id_Documento` " +
         "FROM `tbl_remolques` as `remolques`" +
         "INNER JOIN `tbl_docs_remolques` as `docs`" +
-        //"ON `docs`.`id_Unidad`= `unidades`.`id_Unidad`" +
+        "ON `docs`.`id_Remolque`= `remolques`.`id_Remolque`" +
         //"INNER JOIN  `tbl_tipocandado` as `candado`" +
         //"ON `candado`.`id_Candado`= `unidades`.`id_Candado`" +
         //" AND `candado`.`id_Candado`= `unidades`.`id_Candado`" +
