2c17cb2e1e0893e217b1bd97ea9a374c3ca66b4b
descnz <90636380+descnz@users.noreply.github.com>
2023-05-30 23:57:07 -0600

    Remolques and Mercancias dynamic addition working

--- controllers/createXml.js
+++ controllers/createXml.js
@@ -1,21 +1,43 @@
 /***
  * IMPORTANDO LIBRERIAS PARA HACER QUERIES
  */
-const { sequelize } = require("../config/mysql");
-const { QueryTypes } = require("sequelize");
+const {
+  sequelize
+} = require("../config/mysql");
+const {
+  QueryTypes
+} = require("sequelize");
 
-const { handleHttpResponse } = require("../utils/handleResponse");
-const { handleHttpError } = require("../utils/handleError");
-const { matchedData } = require("express-validator");
+const {
+  handleHttpResponse
+} = require("../utils/handleResponse");
+const {
+  handleHttpError
+} = require("../utils/handleError");
+const {
+  matchedData
+} = require("express-validator");
 var xml2js = require("xml2js");
 
 /******Se invocan las tablas/modelos necesarias para los queries*********/
-const { tiposComprobantesModel } = require("../models");
-const { tiposMonedasModel } = require("../models");
-const { regimenFiscalModel } = require("../models");
-const { metodoPagosModel } = require("../models");
-const { formaPagosModel } = require("../models");
-const { usosCFDIModel } = require("../models");
+const {
+  tiposComprobantesModel
+} = require("../models");
+const {
+  tiposMonedasModel
+} = require("../models");
+const {
+  regimenFiscalModel
+} = require("../models");
+const {
+  metodoPagosModel
+} = require("../models");
+const {
+  formaPagosModel
+} = require("../models");
+const {
+  usosCFDIModel
+} = require("../models");
 
 /******Install the fs  library to  create the file cfdi_YYYY-MM-DD_HH-mm-ss.xml*********/
 var fs = require("fs");
@@ -68,42 +90,53 @@ async function createXmlCtrl(req, res) {
     const st_FechaHoraLlegada = body.st_FechaHoraLlegada;
 
     //const id_DomicilioFiscalReceptor = body.id_DomicilioFiscalReceptor;
-
+    //1 de diciembre de 2022 3 de julio de 2023
     //console.log("dec_TotalDistRec", dec_TotalDistRec);
 
     /******QUERYING DATA RECEIVED FROM JSON, TO POST REAL VALUES ON XML******/
     let st_TipoComprobante = await tiposComprobantesModel.findOne({
-      where: { id_TipoComprobante },
+      where: {
+        id_TipoComprobante
+      },
       attributes: ["st_TipoComprobante"],
     });
     let st_TipoMoneda = await tiposMonedasModel.findOne({
-      where: { id_Moneda: id_TipoMoneda },
+      where: {
+        id_Moneda: id_TipoMoneda
+      },
       attributes: ["c_Moneda"],
     });
 
     let c_RegimenFiscal_emisor = await regimenFiscalModel.findOne({
-      where: { id_RegimenFiscal: id_RegimenFiscal_emisor },
+      where: {
+        id_RegimenFiscal: id_RegimenFiscal_emisor
+      },
       //attributes: ['c_RegimenFiscal']
     });
     let c_RegimenFiscalReceptor = await regimenFiscalModel.findOne({
-      where: { id_RegimenFiscal: id_RegimenFiscalReceptor },
+      where: {
+        id_RegimenFiscal: id_RegimenFiscalReceptor
+      },
     });
     let c_MetodoPago = await metodoPagosModel.findOne({
-      where: { id_MetodoPago: id_MetodoPago },
+      where: {
+        id_MetodoPago: id_MetodoPago
+      },
     });
     /*let c_FormaPago= await formaPagosModel.findOne({
             where: {id_FormaPago},
         });*/
     let c_UsoCFDI = await usosCFDIModel.findOne({
-      where: { id_UsoCFDI },
+      where: {
+        id_UsoCFDI
+      },
     });
 
     /**USING A RAW QUERY FOR US TO NOT CREATE A MODEL AND QUERYING DIRECT FROM THE DATA BASE,
      * WE NEED TO SANITIZE THE INPUT THOUGH!
      */
     let st_ObjetoImp = await sequelize.query(
-      "SELECT c_ObjetoImp FROM c_ObjetoImp WHERE id_ObjetoImp = ?",
-      {
+      "SELECT c_ObjetoImp FROM c_ObjetoImp WHERE id_ObjetoImp = ?", {
         replacements: [id_ObjetoImp],
         type: sequelize.QueryTypes.SELECT,
       }
@@ -130,15 +163,15 @@ async function createXmlCtrl(req, res) {
     const st_Fecha = date.toISOString();
 
     //console.log("test", c_FormaPago);
-
+    
+    
     var JsonStructureCFDI = {
       "cfdi:Comprobante": {
         $: {
           "xmlns:cfdi": "http://www.sat.gob.mx/cfd/4",
           "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
           "xmlns:cartaporte20": "http://www.sat.gob.mx/CartaPorte20",
-          "xsi:schemaLocation":
-            "http://www.sat.gob.mx/cfd/4 http://www.sat.gob.mx/sitio_internet/cfd/4/cfdv40.xsd http://www.sat.gob.mx/CartaPorte20 http://www.sat.gob.mx/sitio_internet/cfd/CartaPorte/CartaPorte20.xsd",
+          "xsi:schemaLocation": "http://www.sat.gob.mx/cfd/4 http://www.sat.gob.mx/sitio_internet/cfd/4/cfdv40.xsd http://www.sat.gob.mx/CartaPorte20 http://www.sat.gob.mx/sitio_internet/cfd/CartaPorte/CartaPorte20.xsd",
           Certificado: "XXXXXX", //NOS LO OTORGA EL SAT DESPUES DE TIMBRAR CFDI
           Fecha: st_Fecha,
           Folio: "CP-1", //uno hasta 40                caracteres alfanuméricos, creo que lo podemos armar nosotros mismos
@@ -172,34 +205,30 @@ async function createXmlCtrl(req, res) {
             UsoCFDI: c_UsoCFDI,
           },
         },
-        "cfdi:Conceptos": [
-          {
-            "cfdi:Concepto": [
-              {
-                $: {
-                  ClaveProdServ: "78101800",
-                  NoIdentificacion: "UT421511",
-                  Cantidad: "1",
-                  ClaveUnidad: "H87",
-                  Unidad: "Pieza",
-                  Descripcion: "Transporte de carga por carretera",
-                  ValorUnitario: "0.00",
-                  ObjetoImp: st_ObjetoImp,
-                  Importe: i_Importe,
-                  /**En este campo se deberá registrar el importe del impuesto
-                        trasladado que aplica al concepto, el cual será el resultado de
-                        multiplicar el valor de la base por la tasa.
+        "cfdi:Conceptos": [{
+          "cfdi:Concepto": [{
+            $: {
+              ClaveProdServ: "78101800",
+              NoIdentificacion: "UT421511",
+              Cantidad: "1",
+              ClaveUnidad: "H87",
+              Unidad: "Pieza",
+              Descripcion: "Transporte de carga por carretera",
+              ValorUnitario: "0.00",
+              ObjetoImp: st_ObjetoImp,
+              Importe: i_Importe,
+              /**En este campo se deberá registrar el importe del impuesto
+                    trasladado que aplica al concepto, el cual será el resultado de
+                    multiplicar el valor de la base por la tasa.
 
-                        Ejemplo:
-                        Base: 10,000.00
-                        Tasa: 0.16
-                        Importe: 1,600.00
-                  **/
-                },
-              },
-            ],
-          },
-        ],
+                    Ejemplo:
+                    Base: 10,000.00
+                    Tasa: 0.16
+                    Importe: 1,600.00
+              **/
+            },
+          }, ],
+        }, ],
         "cfdi:Complemento": {
           "cartaporte20:CartaPorte": {
             $: {
@@ -216,25 +245,12 @@ async function createXmlCtrl(req, res) {
                 UnidadPeso: "XBX",
                 NumTotalMercancias: "1",
               },
-              "cartaporte20:Mercancia": {
-                $: {
-                  BienesTransp: "11121900",
-                  Descripcion: "Accesorios de equipo de telefonía",
-                  Cantidad: "1.0",
-                  ClaveUnidad: "XBX",
-                  PesoEnKg: "1.0",
-                  MaterialPeligroso: "No",
-                },
-                "cartaporte20:CantidadTransporta": {
-                  $: {
-                    Cantidad: "1",
-                    IDOrigen: "OR101010",
-                    IDDestino: "DE202020",
-                  },
-                },
-              },
+              "cartaporte20:Mercancia": [],
               "cartaporte20:Autotransporte": {
-                $: { PermSCT: "TPAF01", NumPermisoSCT: "NumPermisoSCT" },
+                $: {
+                  PermSCT: `test`,
+                  NumPermisoSCT: "NumPermisoSCT"
+                },
 
                 "cartaporte20:IdentificacionVehicular": {
                   $: {
@@ -251,11 +267,6 @@ async function createXmlCtrl(req, res) {
                     PolizaRespCivil: "123456789",
                   },
                 },
-                "cartaporte20:Remolques": {
-                  "cartaporte20:Remolque": {
-                    $: { SubTipoRem: "CTR004", Placa: "VL45K98" },
-                  },
-                },
               },
             },
             "cartaporte20:FiguraTransporte": {
@@ -269,7 +280,6 @@ async function createXmlCtrl(req, res) {
             },
           },
 
-          //TotalDistRec : value with two decimals
         },
       },
     };
@@ -283,104 +293,171 @@ async function createXmlCtrl(req, res) {
 
     var builder = new xml2js.Builder();
 
-    /*var jsonTest=    {
-      "cfdi:Complemento": {
-        "cartaporte20:CartaPorte": {
-          $: {
-            Version: "2.0",
-            TranspInternac: "No",
-            TotalDistRec: dec_TotalDistRec, //DECIMAL(7,2)
-          },
-          "cartaporte20:Ubicaciones": {
-    "cartaporte20:Ubicacion": []
-  }}}}*/
-
     var ubicacionesArray =
       JsonStructureCFDI["cfdi:Comprobante"]["cfdi:Complemento"][
         "cartaporte20:CartaPorte"
       ]["cartaporte20:Ubicaciones"]["cartaporte20:Ubicacion"];
 
 
+    const ubicaciones = req.body.Ubicaciones;
+    const ubicacionesLength = ubicaciones.length;
 
+    console.log(`Length of Ubicaciones array: ${ubicacionesLength}`);
 
-
-
-      const ubicaciones = req.body.Ubicaciones;
-      const ubicacionesLength = ubicaciones.length;
-  
-      console.log(`Length of Ubicaciones array: ${ubicacionesLength}`);
-  
-      for (let i = 0; i < ubicacionesLength; i++) {
-        const ubicacion = ubicaciones[i];
-        if (ubicacion.TipoUbicacion === "Origen") {
-          // Perform the action for "TipoUbicacion" equal to "Origen"
-          console.log(`Processing TipoUbicacion ${i} equal to 'Origen'`);
-          ubicacionesArray.push({
+    for (let i = 0; i < ubicacionesLength; i++) {
+      const ubicacion = ubicaciones[i];
+      if (ubicacion.TipoUbicacion === "Origen") {
+        // Perform the action for "TipoUbicacion" equal to "Origen"
+        console.log(`Processing TipoUbicacion ${i} equal to 'Origen':`);
+        ubicacionesArray.push({
+          $: {
+            // Ubicacion properties here
+            TipoUbicacion: "Origen",
+            RFCRemitenteDestinatario: `${ubicaciones[i].st_RemitenteRFC}`,
+            FechaHoraSalidaLlegada: `${ubicaciones[i].date_FechaSalida}`,
+            IDUbicacion: `${ubicaciones[i].st_IdUbicacion}`,
+            NombreRemitenteDestinatario: `${ubicaciones[i].st_RemitenteNombre}`,
+          },
+          "cartaporte20:Domicilio": {
             $: {
-              // Ubicacion properties here
-              TipoUbicacion: "Origen",
-              RFCRemitenteDestinatario: `${ubicaciones[i].st_RemitenteRFC}`,
-              FechaHoraSalidaLlegada: `${ubicaciones[i].date_FechaSalida}`,
+              // Domicilio properties here
+              Calle: `${ubicaciones[i].Domicilio.st_Calle}`,
+              Colonia: `${ubicaciones[i].Domicilio.id_Colonia}`,
+              Localidad: `${ubicaciones[i].Domicilio.id_Localidad}`,
+              Municipio: `${ubicaciones[i].Domicilio.id_Municipio}`,
+              Estado: `${ubicaciones[i].Domicilio.id_Estado}`,
+              Pais: "MEX",
+              CodigoPostal: `${ubicaciones[i].Domicilio.c_codigoPostal}`,
             },
-            "cartaporte20:Domicilio": {
-              $: {
-                // Domicilio properties here
-                Calle: "calle",
-                NumeroExterior: "214",
-                Colonia: "0347",
-                Localidad: "23",
-                Referencia: "casa blanca",
-                Municipio: "004",
-                Estado: "COA",
-                Pais: "MEX",
-                CodigoPostal: "25350",
-              },
-            }
-          });
-        } else {
-          console.log(`Processing 'TipoUbicacion' ${i} equal to 'Destino'`);
-          ubicacionesArray.push({
+          }
+        });
+      } else {
+        console.log(`Processing 'TipoUbicacion' ${i} equal to 'Destino'`);
+        ubicacionesArray.push({
+          $: {
+            // Ubicacion properties here
+            TipoUbicacion: "Destino",
+            RFCRemitenteDestinatario: `${ubicaciones[i].st_DestinatarioRFC}`,
+            FechaHoraSalidaLlegada: `${ubicaciones[i].st_FechaHoraLlegada}`,
+            DistanciaRecorrida: `${ubicaciones[i].DistanciaRecorrida}`,
+          },
+          "cartaporte20:Domicilio": {
             $: {
-              // Ubicacion properties here
-              TipoUbicacion: "Destino",
-              RFCRemitenteDestinatario: `${ubicaciones[i].st_DestinatarioRFC}`,
-              FechaHoraSalidaLlegada: `${ubicaciones[i].st_FechaHoraLlegada}`,
+              // Domicilio properties here
+              Calle: `${ubicaciones[i].Domicilio.st_Calle}`,
+              Colonia: `${ubicaciones[i].Domicilio.id_Colonia}`,
+              Localidad: `${ubicaciones[i].Domicilio.id_Localidad}`,
+              Municipio: `${ubicaciones[i].Domicilio.id_Municipio}`,
+              Estado: `${ubicaciones[i].Domicilio.id_Estado}`,
+              Pais: "MEX",
+              CodigoPostal: `${ubicaciones[i].Domicilio.c_codigoPostal}`,
+
             },
-            "cartaporte20:Domicilio": {
-              $: {
-                // Domicilio properties here
-                Calle: "calle",
-                NumeroExterior: "214",
-                Colonia: "0347",
-                Localidad: "23",
-                Referencia: "casa blanca",
-                Municipio: "004",
-                Estado: "COA",
-                Pais: "MEX",
-                CodigoPostal: "25350",
-              },
-            }
-          });
-        }
+          }
+        });
       }
+    }
+
+    var mercanciasArray =
+      JsonStructureCFDI["cfdi:Comprobante"]["cfdi:Complemento"][
+        "cartaporte20:CartaPorte"
+      ]["cartaporte20:Mercancias"]["cartaporte20:Mercancia"];
+
+    var autotransporteArray =
+      JsonStructureCFDI["cfdi:Comprobante"]["cfdi:Complemento"][
+        "cartaporte20:CartaPorte"
+      ]["cartaporte20:Mercancias"]["cartaporte20:Autotransporte"];
+
+
+
+      // const autotransportes = req.body.Mercancias.Autotransportes;
+      // const mercanciasLength = mercancias.length;
+      // console.log(`Length of Mercancias array: ${mercanciasLength}`);
+  
+
+    const mercancias = req.body.Mercancias;
+    const mercanciasLength = mercancias.length;
+    console.log(`Length of Mercancias array: ${mercanciasLength}`);
+
+    for (let i = 0; i < mercanciasLength; i++) {
+      const mercancia = mercancias[i];
+      // Perform the action for teach mercancia" equal to "Origen"
+      console.log(`Pushing mercancia number ${i}: ${mercancia}`);
+      mercanciasArray.push({
+        $: {
+          // Mercancia properties here
+          BienesTransp: `${mercancias[i].BienesTransp}`,
+          Descripcion: `${mercancias[i].Descripcion}`,
+          Cantidad: `${mercancias[i].Cantidad}`,
+          ClaveUnidad: `${mercancias[i].ClaveUnidad}`,
+          PesoEnKg: `${mercancias[i].PesoEnKg}`,
+          MaterialPeligroso: `${mercancias[i].MaterialPeligroso}`,
+        },
+        "cartaporte20:CantidadTransporta": {
+          $: {
+            // Domicilio properties here
+            Cantidad: `${mercancias[i].CantidadTransporta.Cantidad}`,
+            IDOrigen: `${mercancias[i].CantidadTransporta.IDOrigen}`,
+            IDDestino: `${mercancias[i].CantidadTransporta.IDDestino}`,
+          },
+        }
+      })
+    };
+
+
+
+    const remolques = req.body.Mercancias[0].Autotransporte.Remolques;
+    console.log(`This is inside the remolques object: ${JSON.stringify(remolques)}`)
+
+    const remolquesLength = remolques.length;
+    console.log(`Length of remolques array: ${remolquesLength}`);
+
+    if (remolquesLength!=0) {
+      JsonStructureCFDI["cfdi:Comprobante"]["cfdi:Complemento"]["cartaporte20:CartaPorte"]["cartaporte20:Autotransporte"]["cartaporte20:Remolques"] = {
+        "cartaporte20:Remolque": [], // puede tener 0-2 remolques
+      };
       
-    
+      var remolquesArray =
+      JsonStructureCFDI["cfdi:Comprobante"]["cfdi:Complemento"][
+        "cartaporte20:CartaPorte"
+      ]["cartaporte20:Mercancias"]["cartaporte20:Autotransporte"]["cartaporte20:Remolques"]["cartaporte20:Remolque"];
+  
+      for (let i = 0; i < remolquesLength; i++) {
+        const remolque = remolques[i];
+        // Perform the action for teach mercancia" equal to "Origen"
+        console.log(`Pushing remolque number ${i}: ${remolque}`);
+        remolquesArray.push(
+          
+          { // Mercancia properties here
+            Placa: `${remolques[i].st_Placa}`,
+            SubTipoRem: `${remolques[i].st_ClaveRemolque}`,
+          }
+          )
+      };
+    }else if(remolquesLength==0){
+      console.log("There are no remolques to add.")
+    }else{
+      console.log(`ERROR: You can only a maximum of 2 remolques, you're tring to add ${remolquesLength} remolques. `)
+    }
+      
+
+
     var xmlRaw = builder.buildObject(JsonStructureCFDI, options);
+    console.log(`this is generated raw XML${xmlRaw}`);
 
-    //var objectConverted = builder.buildObject(jsonTest, options);
-
-    console.log(`this is the arr  array${xmlRaw}`);
-
-    //console.log(xml);
     let xmlFileName = await createTimestampedXmlFile(xmlRaw);
 
     handleHttpResponse(res, {
       xmlRaw: `${xmlRaw}`,
       xmlFileName: `${xmlFileName}`,
     });
+
   } catch (e) {
     console.error(e);
-    res.status(500).json({ success: false, message: "Server Error" });
+    res.status(500).json({
+      success: false,
+      message: "Server Error"
+    });
 
     console.log("Login error: " + e);
     //handleHttpError(res, "ERROR_CREATE_XML");
